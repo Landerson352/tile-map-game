833b41882b89dc3d06dac349ef91f94810fbf864
Lincoln Anderson <landerson@threefivetwo.com>
2020-06-08 00:38:43 -0400

    add gamenew route

--- src/Router.js
+++ src/Router.js
@@ -4,10 +4,12 @@ import { BrowserRouter, Switch } from 'react-router-dom';
 import PrivateRoute from './lib/components/PrivateRoute';
 import Games from './components/Games';
 import Game from './components/Game';
+import GameNew from './components/GameNew';
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
+      <PrivateRoute component={GameNew} path="/game-new/:gameId" exact />
       <PrivateRoute component={Game} path="/game/:gameId" exact />
       <PrivateRoute component={Games} path="/" />
     </Switch>
