011aac32e19a900d2d8f94a7f11bae5dce5872f1
Lincoln Anderson <landerson@threefivetwo.com>
2020-07-03 20:12:33 -0400

    comment console logs

--- src/utils/isTilePlacementValid.js
+++ src/utils/isTilePlacementValid.js
@@ -4,12 +4,12 @@ const isTilePlacementValid = (tile, grid) => {
   const { x, y } = tile;
   const edges = getEdges(tile);
 
-  console.log(edges);
+  // console.log(edges);
 
-  console.log(grid[`${x}_${y - 1}`] && getEdges(grid[`${x}_${y - 1}`]).bottom !== edges.roads.top);
-  console.log(grid[`${x + 1}_${y}`] && getEdges(grid[`${x + 1}_${y}`]).left !== edges.roads.right);
-  console.log(grid[`${x}_${y + 1}`] && getEdges(grid[`${x}_${y + 1}`]).top !== edges.roads.bottom);
-  console.log(grid[`${x - 1}_${y}`] && getEdges(grid[`${x - 1}_${y}`]).right !== edges.roads.left);
+  // console.log(grid[`${x}_${y - 1}`] && getEdges(grid[`${x}_${y - 1}`]).bottom !== edges.roads.top);
+  // console.log(grid[`${x + 1}_${y}`] && getEdges(grid[`${x + 1}_${y}`]).left !== edges.roads.right);
+  // console.log(grid[`${x}_${y + 1}`] && getEdges(grid[`${x}_${y + 1}`]).top !== edges.roads.bottom);
+  // console.log(grid[`${x - 1}_${y}`] && getEdges(grid[`${x - 1}_${y}`]).right !== edges.roads.left);
 
   if (grid[`${x}_${y - 1}`] && getEdges(grid[`${x}_${y - 1}`]).roads.bottom !== edges.roads.top) return false;
   if (grid[`${x + 1}_${y}`] && getEdges(grid[`${x + 1}_${y}`]).roads.left !== edges.roads.right) return false;
